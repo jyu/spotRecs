40dd91abfbbf62c6d174176320df19e0ec84b70a
Jerry Yu <jerryyu8@gmail.com>
2017-03-14 23:58:06 -0400

    change start page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 /**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
+*   Based on Spotify authoritization guide
  */
 // Express.Js, Request, Jquery, Handlebars, Ajax
 
