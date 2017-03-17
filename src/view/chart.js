0414ff50260152ddb9a9ff9556707cafb52db6a1
Jerry Yu <jerryyu8@gmail.com>
2017-03-17 19:51:59 -0400

    edit chart

--- src/view/chart.js
+++ src/view/chart.js
@@ -1,61 +1,23 @@
-Highcharts.chart('container', {
-
+Highcharts.chart('container1', {
+    data: {
+        table: 'datatable'
+    },
     chart: {
         type: 'column'
     },
     title: {
-        text: 'Song Analysis'
-    },
-    subtitle: {
-        text: 'Source: Spotify API'
-    },
-    xAxis: {
-        categories: [
-            'Danceability',
-            'Energy',
-            'Acousticness',
-            'Instrumentalness',
-            'Liveness',
-            'Valence',
-            'Tempo'
-        ],
-        crosshair: true
+        text: 'Data extracted from a HTML table in the page'
     },
     yAxis: {
-        min: 0,
+        allowDecimals: false,
         title: {
-            text: 'Value'
+            text: 'Units'
         }
     },
     tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
+        formatter: function () {
+            return '<b>' + this.series.name + '</b><br/>' +
+                this.point.y + ' ' + this.point.name.toLowerCase();
         }
-    },
-    series: [{
-        name: 'Tokyo',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6]
-
-    }, {
-        name: 'New York',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0]
-
-    }, {
-        name: 'London',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0]
-
-    }, {
-        name: 'Berlin',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4]
-
-    }]
+    }
 });
